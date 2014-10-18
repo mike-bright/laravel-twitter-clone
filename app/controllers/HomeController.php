e838af1fe8108e42d4e6bd4e7645b3d06804ace3
mike-bright <brightvette@gmail.com>
2014-10-18 15:37:18 -0400

    Scrapped unnecessary HomeController logic

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,19 +4,7 @@ class HomeController extends BaseController {
 
 	public function showMain()
 	{
-		$user = User::getCurrent();
-		$posts = Post::fetchAllHomePosts($user)->simplePaginate(10);
-		$followerCount = $user->getFollowersCount();
-		$followingCount = $user->getFollowingCount();
-
-		$viewData = array(
-			'posts' => $posts,
-			'followerCount' => $followerCount,
-			'followingCount' => $followingCount,
-			'user' => $user,
-			);
-
-		return View::make('home.main', $viewData);
+		return View::make('home.main');
 	}
 
 }
