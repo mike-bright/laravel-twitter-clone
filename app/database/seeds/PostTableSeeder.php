b706b99ff8862e57f5052b56bfc3aaf87baae635
mike-bright <brightvette@gmail.com>
2014-10-19 14:23:33 -0400

    Increased post seeding

--- app/database/seeds/PostTableSeeder.php
+++ app/database/seeds/PostTableSeeder.php
@@ -4,17 +4,16 @@ class PostTableSeeder extends seeder {
 	public function run() 
 	{
 		DB::table('post')->delete();
-		$i = 0;
 		
     	foreach (User::all() as $user) {
-            $faker = Faker\Factory::create();
-    		Post::create(array(
-    			'userId' => $user->id,
-    			'text' => $faker->realText(150, 2),
-    			'imageId' => Image::getBlankImage()->id
+            for ($i=0; $i < 10; $i++) { 
+                $faker = Faker\Factory::create();
+        		Post::create(array(
+        			'userId' => $user->id,
+        			'text' => $faker->realText(150, 2),
+        			'imageId' => Image::getBlankImage()->id
     			));
-
-    		$i++;
+            }
     	}
 
 	}
