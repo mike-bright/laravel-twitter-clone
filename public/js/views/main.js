278baab27931b30fefe9f6bda4992d47207a4f34
mike-bright <brightvette@gmail.com>
2014-10-22 17:58:16 -0400

    Fixed posting bug

--- public/js/views/main.js
+++ public/js/views/main.js
@@ -60,6 +60,7 @@ define([
 
 				this.collection.create(new Post({
 					'text': $('#text').val(),
+					'userName': this.currentUser.attributes.userName,
 					'currentUser': 1
 				}));
 				$('#text').val('');
