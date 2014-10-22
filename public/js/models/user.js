df8b9fcd1a86ccdfb1a0496dbc8d66eec7a59e6e
mike-bright <brightvette@gmail.com>
2014-10-21 20:17:52 -0400

    User Model - Added load method

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,6 +1,10 @@
 define(['underscore', 'backbone'], function(_, Backbone) {
 	var UserModel = Backbone.Model.extend({
-		url: '/api/user'
+		url: '/api/user',
+		load: function(id) {
+			this.url = this.url + '?id=' + id;
+			this.fetch();
+		}
 	});
 
 	return UserModel;
